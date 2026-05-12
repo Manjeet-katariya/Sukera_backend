36c7d35fc523f7dd3e6fc98698d75739514acc30
Manjeet Katariya <manjeetkatariya2000@gmail.com>
2026-05-12 21:52:36 +0530

    new commit

--- utils/emailService.js
+++ utils/emailService.js
@@ -268,7 +268,7 @@ const sendThankYouEmail = async (toEmail, name, type = 'contact') => {
             
             <p style="color: #555; font-size: 16px; line-height: 1.8;">
               For urgent queries, you can reach us directly at:<br>
-              <strong style="color: #a68a6b;">Phone: +91 98765 43210</strong><br>
+              <strong style="color: #a68a6b;">Phone: +91 8619633247</strong><br>
               <strong style="color: #a68a6b;">Email: sukeradexterity@gmail.com</strong>
             </p>
             
